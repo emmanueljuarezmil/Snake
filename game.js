d3297560cf02a606ad2587c73528527596d80462
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-10-05 15:48:26 -0300

    working basic without bugs

--- game.js
+++ game.js
@@ -1,15 +1,30 @@
-"use-strict";
+"use strict";
 
 import { 
     update as updateSnake,
     draw as drawSnake,
     SNAKE_SPEED,
- } from './snake.js'
+    getSnakeHead,
+    snakeIntersection
+} from './snake.js';
+import {
+    update as updateFood,
+    draw as drawFood
+} from './food.js';
+import { outsideGrid } from './grid.js';
+
 
 let lastRenderTime = 0;
-const gameBoard = document.getElementById('game-board')
+let gameOver = false;
+const gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
+    if(gameOver) {
+        if (confirm('Has perdido, deseas iniciar una partida nueva?')) {
+            window.location = '/'
+        }
+        return
+    }
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
@@ -22,8 +37,16 @@ window.requestAnimationFrame(main);
 
 function update() {
     updateSnake();
+    updateFood();
+    checkDeath();
 };
 
 function draw() {
+    gameBoard.innerHTML = '';
     drawSnake(gameBoard);
+    drawFood(gameBoard);
 };
+
+function checkDeath() {
+    gameOver = outsideGrid( getSnakeHead() ) || snakeIntersection()
+}
