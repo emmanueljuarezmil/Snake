e92b30dc901935c2ef787b057042ff6b1de074c1
emmanueljuarezmil <emmanueljuarezmil@gmail.com>
2021-11-25 01:13:41 -0300

    indent code

--- game.js
+++ game.js
@@ -1,54 +1,50 @@
-'use strict';
+"use strict";
 
-import { 
-    update as updateSnake,
-    draw as drawSnake,
-    SNAKE_SPEED,
-    getSnakeHead,
-    snakeIntersection
-} from './snake.js';
 import {
-    update as updateFood,
-    draw as drawFood
-} from './food.js';
-import { outsideGrid } from './grid.js';
-
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
+} from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
 
 let lastRenderTime = 0;
 let gameOver = false;
-const gameBoard = document.getElementById('game-board');
+const gameBoard = document.getElementById("game-board");
 
-localStorage.getItem('highScore') ? null : localStorage.setItem('highScore', 0);
+localStorage.getItem("highScore") ? null : localStorage.setItem("highScore", 0);
 
 function main(currentTime) {
-    if(gameOver) {
-        if (confirm('Has perdido, deseas iniciar una partida nueva?')) {
-            window.location = '/'
-        }
-        return
+  if (gameOver) {
+    if (confirm("Has perdido, deseas iniciar una partida nueva?")) {
+      window.location = "/";
     }
-    window.requestAnimationFrame(main);
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
-    lastRenderTime = currentTime;
-    update();
-    draw();
-};
+    return;
+  }
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+  lastRenderTime = currentTime;
+  update();
+  draw();
+}
 
 window.requestAnimationFrame(main);
 
 function update() {
-    updateSnake();
-    updateFood();
-    checkDeath();
-};
+  updateSnake();
+  updateFood();
+  checkDeath();
+}
 
 function draw() {
-    gameBoard.innerHTML = '';
-    drawSnake(gameBoard);
-    drawFood(gameBoard);
-};
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
 
 function checkDeath() {
-    gameOver = outsideGrid( getSnakeHead() ) || snakeIntersection()
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
